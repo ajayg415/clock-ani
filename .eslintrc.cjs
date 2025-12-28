4d8b971f18ab514cff9b9ecfb06f15a1709a6df4
Ajay Gangisetti <ajay.g415@gmail.com>
2025-12-28 16:47:59 +0530

    initial setup with formatters + vite + react

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/anchor-is-valid': 'off'
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>clock-ani</h1>
+        <p>A Vite + React starter. Replace this with your clock animation.</p>
+      </header>
+      <main>
+        <div className="box">Hello from React + Vite!</div>
+      </main>
+    </div>
+  );
+}
